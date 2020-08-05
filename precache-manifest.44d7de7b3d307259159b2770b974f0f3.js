1b3b0d748a50e4bbb9a33a860aaf23faffb4bebd
William Lee <wl433@cornell.edu>
2020-08-05 12:42:01 -0400

    Updates

---
+++ precache-manifest.44d7de7b3d307259159b2770b974f0f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d12c691daed9a14d2e53bdbc631db54d",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "fc49f47bdf28bd530e3a",
+    "url": "/react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b4fb08bf3ef3bd7e0527",
+    "url": "/react-deploy/static/js/2.fe66a864.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.fe66a864.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc49f47bdf28bd530e3a",
+    "url": "/react-deploy/static/js/main.7b889d04.chunk.js"
+  },
+  {
+    "revision": "9799d9da0d0cfae79197",
+    "url": "/react-deploy/static/js/runtime-main.d97cbf54.js"
+  }
+]);
