a7160bda5a87289be86b349504c7a51d6dd31a44
William Lee <wl433@cornell.edu>
2020-08-07 16:54:38 -0400

    hello world

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/header/Header'
 
 function App() {
   return (
     <div className="App">
-      <h1>My React opo</h1>
+      <Header/>
     </div>
   );
 }
---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Header.css';
+
+
+export default function Header(){
+    return(
+        <header>
+            <div className="headerContainer">
+                <h1 className="headerText">Hello World</h1>
+            </div>
+        </header>
+    )
+}
