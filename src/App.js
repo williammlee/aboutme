f313b0f96d96a760ebc9a7f2107cbda603e5bfde
William Lee <wl433@cornell.edu>
2020-08-12 15:38:38 -0400

    changed svg to url

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
 import Header from './components/header/Header'
-import Background from './components/header/background'
 
 function App() {
   return (
     <div className="App">
-      <Background/>
+      <Header />
     </div>
   );
 }
