7ed8e5e06bf15a4fb0dcaa286e384e7205019a1a
William Lee <wl433@cornell.edu>
2020-08-12 12:51:21 -0400

    background change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Header from './components/header/Header'
+import Background from './components/header/background'
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Background/>
     </div>
   );
 }
